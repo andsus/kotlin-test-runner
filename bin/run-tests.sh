704c8b960bd41885165078c9dc0af665dd44a535
Andre Susantin <andre.susantin@google.com>
2021-11-06 19:13:30 -0700

    completed tests and add run-tests run-tests-in-docker.sh

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -17,20 +17,20 @@ exit_code=0
 for test_dir in tests/*; do
     test_dir_name=$(basename "${test_dir}")
     test_dir_path=$(realpath "${test_dir}")
-    #results_file_path="${test_dir_path}/results.json"
-    #expected_results_file_path="${test_dir_path}/expected_results.json"
+    results_file_path="${test_dir_path}/results.json"
+    expected_results_file_path="${test_dir_path}/expected_results.json"
 
-    #bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
+    bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
 
     # Normalize the results file
-    #sed -i "s~${test_dir_path}~/solution~g" "${results_file_path}"
+    sed -i "s~${test_dir_path}~/solution~g" "${results_file_path}"
 
     echo "${test_dir_name}: comparing results.json to expected_results.json"
-    #diff "${results_file_path}" "${expected_results_file_path}"
+    diff "${results_file_path}" "${expected_results_file_path}"
 
-    #if [ $? -ne 0 ]; then
-    #    exit_code=1
-    #fi
+    if [ $? -ne 0 ]; then
+       exit_code=1
+    fi
 done
 
 exit ${exit_code}
